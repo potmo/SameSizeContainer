bc16673157ae2255e62e9ad19362640d954483ce
Nisse Bergman <git@potmo.com>
2020-07-25 15:29:05 +0200

    fix: set platforms to iOS and macOS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SameSizeContainer",
-    //platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15), .iOS(.v12)],
     products: [
         .library(
             name: "SameSizeContainer",
